36f97466fba14d6239f382a2db6e28d4a72632dc
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-04-30 00:32:50 +0200

    fix: add missing exchange

--- src/rabbitmq-wrapper/rabbitmq-wrapper.module.ts
+++ src/rabbitmq-wrapper/rabbitmq-wrapper.module.ts
@@ -25,6 +25,10 @@ import { Config, RabbitMQConfig } from '../common/config/configuration';
               name: 'bptf-listing.handled',
               type: 'fanout',
             },
+            {
+              name: 'bptf-event.created',
+              type: 'direct',
+            },
           ],
           uri: `amqp://${rabbitmqConfig.username}:${rabbitmqConfig.password}@${rabbitmqConfig.host}:${rabbitmqConfig.port}/${rabbitmqConfig.vhost}`,
           prefetchCount: 1,
