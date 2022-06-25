027d5fc6e2cb9a536073ef381412af0868a8c12a
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-06-25 13:47:28 +0200

    feat: add queue health indicator

---
+++ src/health/bull.health.ts
@@ -0,0 +1,32 @@
+import { InjectQueue } from '@nestjs/bull';
+import { Injectable } from '@nestjs/common';
+import {
+  HealthIndicator,
+  HealthIndicatorResult,
+  HealthCheckError,
+} from '@nestjs/terminus';
+import { Queue } from 'bull';
+
+@Injectable()
+export class BullHealthIndicator extends HealthIndicator {
+  constructor(
+    @InjectQueue('get-listing')
+    private readonly queue: Queue,
+  ) {
+    super();
+  }
+
+  isHealthy(key: string): Promise<HealthIndicatorResult> {
+    return this.queue.client
+      .ping()
+      .then(() => {
+        return this.getStatus(key, true);
+      })
+      .catch((err) => {
+        throw new HealthCheckError(
+          'Bull check failed',
+          this.getStatus(key, false, { message: err.message }),
+        );
+      });
+  }
+}
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -3,10 +3,18 @@ import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { RabbitMQHealthIndicator } from './rabbitmq.health';
 import { RabbitMQWrapperModule } from '../rabbitmq-wrapper/rabbitmq-wrapper.module';
+import { BullModule } from '@nestjs/bull';
+import { BullHealthIndicator } from './bull.health';
 
 @Module({
-  imports: [TerminusModule, RabbitMQWrapperModule],
-  providers: [RabbitMQHealthIndicator],
+  imports: [
+    TerminusModule,
+    BullModule.registerQueue({
+      name: 'get-listing',
+    }),
+    RabbitMQWrapperModule,
+  ],
+  providers: [BullHealthIndicator, RabbitMQHealthIndicator],
   controllers: [HealthController],
 })
 export class HealthModule {}
